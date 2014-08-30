432bf3d3c903d77c29f69206155a1aed6d891daa
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-08-30 10:17:36 -0500

    Wild OTA: Nicer lookin date

--- flashable/tools/updatewild.sh
+++ flashable/tools/updatewild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 latestversion=009
-latestdate=140821
-latestdateliteral='Thursday, August 21 14'
+latestdate=140830
+latestdateliteral='August 30 2014'
 latestDL=http://forum.xda-developers.com/showthread.php?t=2765196
 input="$1"
 if [[ -z "$input" ]]
