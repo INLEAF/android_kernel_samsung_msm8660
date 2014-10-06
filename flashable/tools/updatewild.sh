888ea5298556e005cedd3b59963aee6007ba56a1
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-10-05 20:12:07 -0500

    derpy

--- flashable/tools/updatewild.sh
+++ flashable/tools/updatewild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 latestversion=010
-latestdate=141004
-latestdateliteral='October 04 2014'
+latestdate=141005
+latestdateliteral='October 05 2014'
 latestDL=http://forum.xda-developers.com/showthread.php?t=2765196
 input="$1"
 if [[ -z "$input" ]]
