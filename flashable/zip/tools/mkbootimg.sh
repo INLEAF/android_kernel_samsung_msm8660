4729a4376032d064f1be5529f2538db98ae63584
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-10-09 23:00:50 -0500

    WildKernel: Use proper celox base address

--- flashable/zip/tools/mkbootimg.sh
+++ flashable/zip/tools/mkbootimg.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 echo \#!/sbin/sh > /tmp/createnewboot.sh
-echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x48000000 --ramdisk_offset 0x01400000 --output /tmp/newboot.img >> /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x40400000 --ramdisk_offset 0x01400000 --output /tmp/newboot.img >> /tmp/createnewboot.sh
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
 rm -rf /sdcard/WildKernel-boot.img
